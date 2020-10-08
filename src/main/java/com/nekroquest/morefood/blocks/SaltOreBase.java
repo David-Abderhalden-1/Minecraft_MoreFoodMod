d02ef477ad356492a1be113950f9f914c075228f
David-Abderhalden-1 <david.abderhalden@hotmail.com>
2020-10-08 22:36:11 +0200

    Started Ore Gen

--- src/main/java/com/nekroquest/morefood/blocks/SaltOreBase.java
+++ src/main/java/com/nekroquest/morefood/blocks/SaltOreBase.java
@@ -1,12 +1,15 @@
 package com.nekroquest.morefood.blocks;
 
 import net.minecraft.block.AbstractBlock;
-import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.OreBlock;
 import net.minecraft.block.SoundType;
 import net.minecraft.block.material.Material;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorldReader;
 import net.minecraftforge.common.ToolType;
 
-public class SaltOreBase extends Block {
+public class SaltOreBase extends OreBlock {
 
     public SaltOreBase() {
         super(AbstractBlock.Properties.create(Material.ROCK)
@@ -16,5 +19,9 @@ public class SaltOreBase extends Block {
                 .harvestTool(ToolType.PICKAXE)
                 .func_235861_h_()
         );
+    }
+    @Override
+    public int getExpDrop(BlockState state, IWorldReader reader, BlockPos pos, int fortune, int silktouch) {
+            return 1;
     }
 }
